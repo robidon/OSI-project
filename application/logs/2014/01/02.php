71d3ef9838a830e3a79ed7170c613bfc9512698b
Rodion <trofimov.rodion@gmail.com>
2018-05-09 23:10:02 +0300

    Delete 02.php

--- application/logs/2014/01/02.php
+++
@@ -1,3 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.'); ?>
-
-2014-01-02 11:03:39 --- ERROR: ErrorException [ 1 ]: Maximum execution time of 30 seconds exceeded ~ MODPATH\database\classes\kohana\database\mysql.php [ 171 ]
